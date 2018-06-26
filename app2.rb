9f6e33be36c3ec37365504db6c308b5a8bcfcf9e
manibrar <manvirbrar1988@gmail.com>
2018-06-26 22:22:10 +0100

    basic calc oop

---
+++ app2.rb
@@ -0,0 +1,28 @@
+class User
+  def initialize (choice)
+      @choice = choice
+  class basic_calc
+    def initialize (number1, number2, add, subtract, multiply, divide)
+      @number1 = 0
+      @number2 = 0
+      def add(@number1, @nummber2, answer)
+        @number1 + @number2 = answer
+      end
+    end
+  end
+end
+
+User.choice.new = gets.chomp
+
+u = User.new('advanced')
+
+puts "#{User.choice}"
+
+# def greeting
+#   puts "Welcome to manyCalc, which function would you like [basic] [advanced] or [health]?"
+#   User_choice = gets.chomp
+#   puts "#{User.choice}"
+# end
+
+
+# greeting
